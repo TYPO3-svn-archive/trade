6b98b1d2c57cec8a1923ffa7c6704e77c1598ed4
irishflute <irishflute@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-04 12:13:10 +0000

    remove static subdir of plugin containing unused CSS editor config
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/trade/trunk@11700 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,11 +13,5 @@ t3lib_extMgm::addUserTSConfig('
 	options.saveDocNew.tx_trade_order_status=1
 ');
 
-  ## Extending TypoScript from static template uid=43 to set up userdefined tag:
-t3lib_extMgm::addTypoScript($_EXTKEY,'editorcfg','
-	tt_content.CSS_editor.ch.tx_trade_pi1 = < plugin.tx_trade_pi1.CSS_editor
-',43);
-
-
 t3lib_extMgm::addPItoST43($_EXTKEY,'pi1/class.tx_trade_pi1.php','_pi1','list_type',0);
 ?>
