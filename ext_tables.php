820f81e1428f3b57be36dc1c9b56069e0f8b69dc
irishflute <irishflute@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-05 02:31:41 +0000

    replace inclusion of flexform after kickstarter mods
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/trade/trunk@11729 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -130,11 +130,11 @@ $TCA["tx_trade_manufacturers"] = array (
 
 
 t3lib_div::loadTCA('tt_content');
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key';
-
+$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,recursive';
+$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
 
 t3lib_extMgm::addPlugin(array('LLL:EXT:trade/locallang_db.xml:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-
+t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:trade/flexform_ds_pi1.xml');
 
 t3lib_extMgm::addStaticFile($_EXTKEY,"pi1/static/","Trade");
 
